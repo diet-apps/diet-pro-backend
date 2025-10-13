9054b3c124db293f87c0d0ffb0242311a2fc186f
LuanSantos00 <luancosmefilho@gmail.com>
2025-10-13 20:47:04 -0300

    adding WebhooksController

---
+++ Diet.Pro.AI/Diet.Pro.AI/Api/Controllers/WebhooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diet.Pro.AI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhooksController : ControllerBase
+    {
+    }
+}
