8a818d9a7d616267f82fe76e0acc9c15bacc0a2f
LuanSantos00 <luancosmefilho@gmail.com>
2025-10-14 22:03:09 -0300

    removing keys

--- Diet.Pro.AI/Diet.Pro.AI/Program.cs
+++ Diet.Pro.AI/Diet.Pro.AI/Program.cs
@@ -2,6 +2,12 @@ using Diet.Pro.AI.Infra.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
