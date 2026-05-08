438691a312f059fae43ddc9fcb37039a43bec42f
LuanSantos00 <luancosmefilho@gmail.com>
2026-05-07 21:10:22 -0300

    model to EmailService

---
+++ Diet.Pro.AI/Diet.Pro.AI/Aplication/Common/Dtos/EmailMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Diet.Pro.AI.Aplication.Common.Dtos
+{
+    public class EmailMessage
+    {
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public bool IsHtml { get; set; } = true;
+    }
+
+}
