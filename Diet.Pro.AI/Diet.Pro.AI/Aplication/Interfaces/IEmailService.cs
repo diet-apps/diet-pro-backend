414c6a5a4a176ed2d5ba1782f8c10769e89a084b
LuanSantos00 <luancosmefilho@gmail.com>
2026-05-11 21:10:02 -0300

    adding IEmailService

---
+++ Diet.Pro.AI/Diet.Pro.AI/Aplication/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Diet.Pro.AI.Aplication.Common.Dtos;
+
+namespace Diet.Pro.AI.Aplication.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailMessage message);
+    }
+}
