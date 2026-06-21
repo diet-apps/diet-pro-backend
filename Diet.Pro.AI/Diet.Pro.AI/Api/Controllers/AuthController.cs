cd0377894146f456b8a03bfa03b8ac5ce0dac7cb
LuanSantos00 <luancosmefilho@gmail.com>
2026-06-21 12:21:25 -0300

    Add ProducesResponseType in controller method

--- Diet.Pro.AI/Diet.Pro.AI/Api/Controllers/AuthController.cs
+++ Diet.Pro.AI/Diet.Pro.AI/Api/Controllers/AuthController.cs
@@ -14,6 +14,8 @@ namespace Diet.Pro.AI.Api.Controllers
         public AuthController(IMediator mediator) => _mediator = mediator;
 
         [HttpPost("login")]
+        [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
             var result = await _mediator.Send(new LoginCommand(request));
